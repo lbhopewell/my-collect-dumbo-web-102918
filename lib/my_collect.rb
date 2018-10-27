97d37b70cfe8f906408c1ec29b48232b101dfb4d
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-27 01:22:48 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+require "pry"
 
+def my_collect(array)
+  new_array = []
+  i = 0 
+  while i < array.length
+  yield (array[i])
+  i = i + 1
+end
+new_array
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+new_array = []
+my_collect(array) do |name|
+  name.split(" ").first 
+
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
